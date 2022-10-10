e62e3be5ee73c3cd6cf5d5f4852fe2041c2f2ad0
jiaLEE <jia9510@naver.com>
2022-10-11 00:52:18 +0900

    spring interceptor - request log.

--- src/main/java/hello/login/web/WebConfig.java
+++ src/main/java/hello/login/web/WebConfig.java
@@ -2,16 +2,28 @@ package hello.login.web;
 
 import hello.login.web.filter.LogFilter;
 import hello.login.web.filter.LoginCheckFilter;
+import hello.login.web.interceptor.LogInterceptor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
-    
-    @Bean
+public class WebConfig implements WebMvcConfigurer {
+
+    //spring이 제공하는 방식
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");    //다 거르지만 이것들은 거르지 않는다.
+    }
+
+    //@Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
         filterFilterRegistrationBean.setFilter(new LogFilter());
